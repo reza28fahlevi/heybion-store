cf80d742903c4971f0c74eee02b1a977c9cb4eee
reza28fahlevi <rezaahmad280401@gmail.com>
2024-08-08 20:57:31 +0700

    update

--- app/Views/Admin/Layout/Footer.php
+++ app/Views/Admin/Layout/Footer.php
@@ -18,17 +18,17 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="<?=site_url()?>/assets/vendor/apexcharts/apexcharts.min.js"></script>
-<script src="<?=site_url()?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?=site_url()?>/assets/vendor/chart.js/chart.umd.js"></script>
-<script src="<?=site_url()?>/assets/vendor/echarts/echarts.min.js"></script>
-<script src="<?=site_url()?>/assets/vendor/quill/quill.min.js"></script>
-<script src="<?=site_url()?>/assets/vendor/simple-datatables/simple-datatables.js"></script>
-<script src="<?=site_url()?>/assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="<?=site_url()?>/assets/vendor/php-email-form/validate.js"></script>
+<script src="<?=site_url()?>assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="<?=site_url()?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=site_url()?>assets/vendor/chart.js/chart.umd.js"></script>
+<script src="<?=site_url()?>assets/vendor/echarts/echarts.min.js"></script>
+<script src="<?=site_url()?>assets/vendor/quill/quill.min.js"></script>
+<script src="<?=site_url()?>assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="<?=site_url()?>assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="<?=site_url()?>assets/vendor/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
-<script src="<?=site_url()?>/assets/js/main.js"></script>
+<script src="<?=site_url()?>assets/js/main.js"></script>
 <script>
   function capitalizeFirstLetter(string) {
       if (!string) return string; // Handle empty strings
--- app/Views/Admin/Layout/Header.php
+++ app/Views/Admin/Layout/Header.php
@@ -13,28 +13,28 @@
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.17.2/dist/sweetalert2.min.js"></script>
   <script src="https://cdn.datatables.net/2.1.2/js/dataTables.js"></script>
   <!-- Favicons -->
-  <link href="<?=site_url()?>/assets/img/favicon.png" rel="icon">
-  <link href="<?=site_url()?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?=site_url()?>assets/img/favicon.png" rel="icon">
+  <link href="<?=site_url()?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?=site_url()?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?=site_url()?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?=site_url()?>/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?=site_url()?>/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="<?=site_url()?>/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="<?=site_url()?>/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?=site_url()?>/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/vendor/simple-datatables/style.css" rel="stylesheet">
   
   <!-- Sweet alert -->
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/sweetalert2@9.17.2/dist/sweetalert2.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/2.1.2/css/dataTables.dataTables.css" />
 
   <!-- Template Main CSS File -->
-  <link href="<?=site_url()?>/assets/css/style.css" rel="stylesheet">
+  <link href="<?=site_url()?>assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin
@@ -52,7 +52,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="<?=site_url()?>/assets/img/logo.png" alt="">
+        <img src="<?=site_url()?>assets/img/logo.png" alt="">
         <span class="d-none d-lg-block">HeyBion Store</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -70,7 +70,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="<?=site_url()?>/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <img src="<?=site_url()?>assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">Admin</span>
           </a><!-- End Profile Iamge Icon -->
 
